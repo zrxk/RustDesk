05479f5dd2c8ad685154f2441a2ca37a3b51484b
zrxk <kimraid@gmail.com>
2024-10-27 20:18:49 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["zrga.us.kgwDHT4v8VT9sXfmR5xHnG9IooRDtOjg8mTW9jUBOO4wg="];
+pub const PUBLIC_RS_PUB_KEY: &str = "wDHT4v8VT9sXfmR5xHnG9IooRDtOjg8mTW9jUBOO4wg=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
