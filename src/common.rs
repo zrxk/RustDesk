60d76ca0b59cd4cdd8b436ba6d58f459c02b6ba7
zrxk <kimraid@gmail.com>
2024-11-17 22:51:40 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -909,7 +909,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://zrga.us.kg:21114".to_owned()
+    "http://990091.xyz:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
